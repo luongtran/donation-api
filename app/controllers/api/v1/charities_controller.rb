8b6d44c67e1a3fa3e29bb8a83010d5ee15698e1a
luongtran <luongtranduc@gmail.com>
2017-04-06 21:45:42 +0700

    fix error

--- app/controllers/api/v1/charities_controller.rb
+++ app/controllers/api/v1/charities_controller.rb
@@ -2,8 +2,8 @@ class Api::V1::CharitiesController < Api::BaseController
 	before_action :authenticate_user!, only: [:index]
 
 	def index
-		if param[:charity_location_id]
-			@charities = Charity.where(charity_location_id: param[:charity_location_id], status: true)
+		if params[:charity_location_id]
+			@charities = Charity.where(charity_location_id: params[:charity_location_id], status: true)
 		else
 			@charities = Charity.where(status: true)
 		end
