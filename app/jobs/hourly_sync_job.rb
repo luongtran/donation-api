d59f33880426e9ff9aad033fa7b55d29f38d2fd7
luongtran <luongtranduc@gmail.com>
2017-04-15 15:27:02 +0700

    add job

--- app/jobs/hourly_sync_job.rb
+++ app/jobs/hourly_sync_job.rb
@@ -4,6 +4,7 @@ class HourlySyncJob
 	include Sidekiq::Worker
 
 	def perform
+		logger.info "Hour Sync Job Run"
 	    donations = Donation.where(sync_status: false)
 	    donations.each do |donation|
 	    	SyncDonationJobJob.perform_later donation
